110f3db35ed37813e66d36c893f134469b2fc700
Muhammed Adnan K <73103641+muhammedadnank@users.noreply.github.com>
2026-01-03 21:47:48 +0530

    Clean up database settings in settings.py
    Removed commented-out database configuration section.

--- config/settings.py
+++ config/settings.py
@@ -47,10 +47,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
